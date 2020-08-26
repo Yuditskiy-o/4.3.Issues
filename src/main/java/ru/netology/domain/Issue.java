53dd8b645ddf099d706b107541b3800bd7feaea1
Yuditskiy-o <Komekadze@gmail.com>
2020-08-26 20:34:18 +0700

    Added Issue logic

--- src/main/java/ru/netology/domain/Issue.java
+++ src/main/java/ru/netology/domain/Issue.java
@@ -1,4 +1,24 @@
 package ru.netology.domain;
 
-public class Issue {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Issue implements Comparable <Issue> {
+    private int id;
+    private boolean isOpen;
+    private String author;
+    private int whenOpened;
+    private Set<String> issueLabels = new HashSet<>();
+    private Set<String> issueAssignees = new HashSet<>();
+
+    @Override
+    public int compareTo(Issue o) {
+        return whenOpened - o.whenOpened;
+    }
 }
