f488dde10560f1e294dcd3439be8edb51d89f365
Yuditskiy-o <Komekadze@gmail.com>
2020-08-26 21:25:28 +0700

    Added Repository and some Manager logic

--- src/main/java/ru/netology/manager/IssueManager.java
+++ src/main/java/ru/netology/manager/IssueManager.java
@@ -1,4 +1,40 @@
 package ru.netology.manager;
 
+import ru.netology.domain.Issue;
+import ru.netology.repository.IssueRepository;
+
+import java.util.*;
+import java.util.function.Predicate;
+
 public class IssueManager {
+    private IssueRepository repository;
+
+    public IssueManager(IssueRepository repository) {
+        this.repository = repository;
+    }
+
+    public void add(Issue issue) {
+        repository.save(issue);
+    }
+
+    private List<Issue> filterBy(Predicate<Issue> predicate) {
+        List<Issue> result = new ArrayList<>();
+        for (Issue issue : repository.findAll()) {
+            if (predicate.test(issue))
+                result.add(issue);
+        }
+        return result;
+    }
+
+    public List<Issue> findOpened() {
+        return filterBy(issue -> issue.isOpen());
+    }
+
+    public List<Issue> findClosed() {
+        return filterBy(issue -> !issue.isOpen());
+    }
+
+
+
+
 }
--- src/main/java/ru/netology/repository/IssueRepository.java
+++ src/main/java/ru/netology/repository/IssueRepository.java
@@ -1,4 +1,38 @@
 package ru.netology.repository;
 
+import ru.netology.domain.Issue;
+
+import java.util.*;
+
 public class IssueRepository {
+    private List<Issue> issues = new ArrayList<>();
+
+    public void addAll(List<Issue> issues) {
+        this.issues.addAll(issues);
+    }
+
+    public List<Issue> findAll() {
+        return issues;
+    }
+
+    public void save(Issue issue) {
+        issues.add(issue);
+    }
+
+    public Issue findById(int id) {
+        for (Issue issue : issues) {
+            if (issue.getId() == id) {
+                return issue;
+            }
+        }
+        return null;
+    }
+
+    public void removeById(int id) {
+        issues.removeIf(issue -> issue.getId() == id);
+    }
+
+    public void removeAll() {
+        issues.clear();
+    }
 }
